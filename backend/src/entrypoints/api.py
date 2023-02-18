cbbbabaa6fe0e3dca99f47d5c78b31ac67bbb532
ekrasnikov <e.krasnikov@redmadrobot.com>
2023-02-19 00:14:20 +0700

    FA-1: add base structure

---
+++ backend/src/entrypoints/api.py
@@ -0,0 +1,39 @@
+import uvicorn
+from fastapi import FastAPI
+from starlette.middleware import Middleware
+from starlette_context.middleware import RawContextMiddleware
+
+import application
+
+from configs.api import config
+
+middleware = [Middleware(RawContextMiddleware)]
+
+app = FastAPI(
+    title="Finance App",
+    version=config.version,
+    docs_url="/docs/api",
+    redoc_url="/redoc/api",
+    openapi_url="/docs/api/openapi.json",
+    middleware=middleware,
+)
+
+
+@app.on_event("startup")
+async def on_startup() -> None:
+    await application.on_startup()
+
+
+@app.on_event("shutdown")
+async def on_shutdown() -> None:
+    await application.on_shutdown()
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "__main__:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+        forwarded_allow_ips="*",
+    )
