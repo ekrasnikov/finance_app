c7dfc20bde10cb22242569db76cb9d623155a4b6
ekrasnikov <e.krasnikov@redmadrobot.com>
2023-02-18 23:51:46 +0700

    add linters and setup.py

---
+++ backend/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name='finance_app',
+    version='',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='ekrasnikov',
+    author_email='',
+    description='',
+    python_requires='==3.11.*'
+)
